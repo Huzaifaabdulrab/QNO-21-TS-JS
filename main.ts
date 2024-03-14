1b9038d290eaf78d9f4d458f0860dc72bb3f6686
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 10:50:38 +0500

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
-
-// task number 21
-
-let person :{name : string , fname : string , age : number}={name:"Huzaifa", fname:"Abdulrab" , age : 16};
-console.log(person)
